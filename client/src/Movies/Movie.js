953d39c75a93702e22f420c1049631e71f70c88c
josh182014 <josh182014@gmail.com>
2019-05-06 15:24:12 -0600

    added save movie functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -27,18 +27,20 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    console.log(addToSavedList)
+    addToSavedList(this.state.movie)
+  }
   
   render() {
+    console.log(this.props)
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
@@ -62,7 +64,7 @@ export default class Movie extends Component {
             </div>
           ))}
         </div>
-        <div className="save-button">Save</div>
+        <div onClick={this.saveMovie} className="save-button">Save</div>
       </div>
     );
   }
