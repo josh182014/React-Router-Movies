270ec2a2e43824bc22052722235adde8f763a7dd
josh182014 <josh182014@gmail.com>
2019-05-06 14:55:39 -0600

    made the cards less ugly for being links

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movie/:id" component={Movie} />
+        <Route path="/movies/:id" component={Movie} />
       </div>
     );
   }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,7 +35,7 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to={`/movie/${movie.id}`}>
+    <Link to={`/movies/${movie.id}`} className='link'>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
